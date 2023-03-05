a284a88bff177ccd5b6915dac21541aa93b72f18
WibuSOS <vincent.joyan@gmail.com>
2023-03-05 14:49:50 +0700

    [FIX] skala rental currency money to decimal

--- backend/models/skalaRental.go
+++ backend/models/skalaRental.go
@@ -5,20 +5,20 @@ import "time"
 type SkalaRentalTab struct {
 	Custcode      string    `json:"custcode" gorm:"not null;type:varchar(25)"`
 	Counter       uint8     `json:"counter" gorm:"type:smallint"`
-	OsBalance     float64   `json:"os_balance" gorm:"type:money"`
-	EndBalance    float64   `json:"end_balance" gorm:"type:money"`
+	OsBalance     float64   `json:"os_balance" gorm:"type:decimal(16,2)"`
+	EndBalance    float64   `json:"end_balance" gorm:"type:decimal(16,2)"`
 	DueDate       time.Time `json:"due_date" gorm:"type:timestamp"`
 	EffRate       float32   `json:"eff_rate" gorm:"type:float"`
-	Rental        float64   `json:"rental" gorm:"type:money"`
-	Principle     float64   `json:"principle" gorm:"type:money"`
-	Interest      float64   `json:"interest" gorm:"type:money"`
+	Rental        float64   `json:"rental" gorm:"type:decimal(16,2)"`
+	Principle     float64   `json:"principle" gorm:"type:decimal(16,2)"`
+	Interest      float64   `json:"interest" gorm:"type:decimal(16,2)"`
 	Inputdate     time.Time `json:"inputdate" gorm:"column:inputdate;type:timestamp"`
 	Inputby       string    `json:"inputby" gorm:"column:inputby;type:varchar(50)"`
 	Lastmodified  time.Time `json:"lastmodified" gorm:"column:lastmodified;type:timestamp"`
 	Modifiedby    string    `json:"modifiedby" gorm:"column:modifiedby;type:varchar(50)"`
 	PaymentDate   time.Time `json:"payment_date" gorm:"type:timestamp"`
-	Penalty       float64   `json:"penalty" gorm:"type:money"`
-	PaymentAmount float64   `json:"payment_amount" gorm:"type:money"`
+	Penalty       float64   `json:"penalty" gorm:"type:decimal(16,2)"`
+	PaymentAmount float64   `json:"payment_amount" gorm:"type:decimal(16,2)"`
 	PaymentType   uint8     `json:"payment_type" gorm:"type:smallint"`
 }
 
